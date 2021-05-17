36d74449fbedee61974d1c077b691d79a2fdc4d1
Fred Blanc <findel@free.fr>
2021-05-17 15:09:58 +0200

    Resolution FizzBuzz

--- FizzBuzz/src/FizzBuzz.php
+++ FizzBuzz/src/FizzBuzz.php
@@ -9,11 +9,29 @@ class FizzBuzz
     /** @return string[] */
     public function range(int $start, int $end): array
     {
-        return [];
+        return array_map(
+            fn($number) => $this->translate($number),
+            range($start, $end)
+        );
     }
 
     public function translate(int $number): string
     {
-        return '';
+        $multiple3 = (($number % 3) === 0);
+        $multiple5 = (($number % 5) === 0);
+
+        if ($multiple3 && $multiple5) {
+            return 'fizzbuzz';
+        }
+
+        if ($multiple3) {
+            return 'fizz';
+        }
+
+        if ($multiple5) {
+            return 'buzz';
+        }
+
+        return (string) $number;
     }
 }
--- FizzBuzz/tests/FizzBuzzTest.php
+++ FizzBuzz/tests/FizzBuzzTest.php
@@ -18,10 +18,44 @@ class FizzBuzzTest extends TestCase
         $this->assertSame('2', $result);
     }
 
+    public function testRange(): void
+    {
+        $fizzBuzz = new FizzBuzz();
+
+        $result = $fizzBuzz->range(1, 2);
+
+        $this->assertSame(['1', '2'], $result);
+    }
+
+    public function testFizz(): void
+    {
+        $fizzBuzz = new FizzBuzz();
+
+        $result = $fizzBuzz->translate(9);
+
+        $this->assertSame('fizz', $result);
+    }
+
+    public function testBuzz(): void
+    {
+        $fizzBuzz = new FizzBuzz();
+
+        $result = $fizzBuzz->translate(10);
+
+        $this->assertSame('buzz', $result);
+    }
+
+    public function testFizzBuzz(): void
+    {
+        $fizzBuzz = new FizzBuzz();
+
+        $result = $fizzBuzz->translate(15);
+
+        $this->assertSame('fizzbuzz', $result);
+    }
+
     public function testSample(): void
     {
-        $this->markTestSkipped();
-
         $fizzBuzz = new FizzBuzz();
 
         $result = implode(', ', $fizzBuzz->range(1, 15));
